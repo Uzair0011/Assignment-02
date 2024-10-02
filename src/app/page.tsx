37e089c98aecc12274006f84283e8c55afddec08
Uzair Ahmed <159642641+Uzair0011@users.noreply.github.com>
2024-10-02 07:26:47 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ export default function Home() {
       <div className="childContainer">
         Hey,
         <br />
-        I'm <span className="color1">Uzair Ahmed</span>
+<h1>       Uzair Ahmed
+</h1>
         <div className="become"> & become a</div>
-        <span className="color2">Web Developer</span>
+        Web Designer
       </div>
       <div className="childContainer">
         <img
-          src="https://static.thenounproject.com/png/553763-200.png"
+          src="https://media.philstar.com/photos/2024/02/12/4_2024-02-12_22-18-20.jpg"
           alt="Uzair picture Shahow"
         />
       </div>
